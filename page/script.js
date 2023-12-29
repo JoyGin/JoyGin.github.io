04cc318dc3ff6ed3d516a4109ba4e471ec9c21b4
JoyGin <1905721262@qq.com>
2023-12-29 12:10:24 +0800

    添加二次认证界面

--- page/script.js
+++ page/script.js
@@ -2,5 +2,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   // 你的代码
   console.log('页面已加载');
-  window.location.href = 'ioazt://mini-iam/auth/sso';
+  //  window.location.href = 'ioazt://mini-iam/auth/sso';
 });
